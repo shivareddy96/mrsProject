001148b7e6ef17cf9da307d2b295efd4be6eeff6
matthewmartinez24 <43081613+matthewmartinez24@users.noreply.github.com>
2018-12-04 22:17:39 -0600

    Create SeedController.cs

---
+++ mrsProject/Controllers/SeedController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using mrsProject.Seeding;
+using mrsProject.DAL;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace mrsProject.Controllers
+{
+    public class SeedController : Controller
+    {
+        private AppDbContext _db;
+
+        public SeedController(AppDbContext context)
+        {
+            _db = context;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SeedGenres()
+        {
+            try
+            {
+                Seeding.SeedGenres.SeedAllGenres(_db);
+            }
+            catch (NotSupportedException ex)
+            {
+                return View("Error", new String[] { "The data has already been added", ex.Message });
+            }
+            catch (InvalidOperationException ex)
+            {
+                return View("Error", new String[] { "There was an error adding data to the database", ex.Message });
+            }
+
+            return View("Confirm");
+        }
+
+        public IActionResult SeedBooks()
+        {
+            try
+            {
+                Seeding.SeedBooks.SeedAllBooks(_db);
+            }
+            catch (NotSupportedException ex)
+            {
+                return View("Error", new String[] { "The data has already been added", ex.Message});
+            }
+            catch (InvalidOperationException ex)
+            {
+                return View("Error", new String[] { "There was an error adding data to the database", ex.Message });
+            }
+
+            return View("Confirm");
+        }
+    }
+}
