7e5c54524233a9a4bfcb79eae2ff4ca8ddd58454
matthewmartinez24 <43081613+matthewmartinez24@users.noreply.github.com>
2018-12-04 22:17:46 -0600

    Create 20181108031431_InitialCreate.cs

---
+++ mrsProject/Migrations/20181108031431_InitialCreate.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mrsProject.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Discounts",
+                columns: table => new
+                {
+                    CouponCode = table.Column<string>(nullable: false),
+                    DiscountDescription = table.Column<string>(nullable: true),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Discounts", x => x.CouponCode);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Inventories",
+                columns: table => new
+                {
+                    InventoryID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    ReorderPrice = table.Column<decimal>(nullable: false),
+                    InventoryQuantity = table.Column<int>(nullable: false),
+                    ReorderPoint = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Inventories", x => x.InventoryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Author = table.Column<string>(nullable: true),
+                    ItemInStock = table.Column<bool>(nullable: false),
+                    SimpleRating = table.Column<decimal>(nullable: false),
+                    ActiveStatus = table.Column<bool>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    BookDescription = table.Column<string>(nullable: true),
+                    PublicationDate = table.Column<DateTime>(nullable: false),
+                    InventoryID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookID);
+                    table.ForeignKey(
+                        name: "FK_Books_Inventories_InventoryID",
+                        column: x => x.InventoryID,
+                        principalTable: "Inventories",
+                        principalColumn: "InventoryID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    ActiveStatus = table.Column<bool>(nullable: false),
+                    City = table.Column<string>(nullable: true),
+                    State = table.Column<string>(nullable: true),
+                    Zipcode = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    CreditCard1 = table.Column<string>(nullable: true),
+                    CreditCard2 = table.Column<string>(nullable: true),
+                    CreditCard3 = table.Column<string>(nullable: true),
+                    InventoryID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Email);
+                    table.ForeignKey(
+                        name: "FK_Users_Inventories_InventoryID",
+                        column: x => x.InventoryID,
+                        principalTable: "Inventories",
+                        principalColumn: "InventoryID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    OrderDate = table.Column<DateTime>(nullable: false),
+                    Subtotal = table.Column<decimal>(nullable: false),
+                    ShippingCost = table.Column<decimal>(nullable: false),
+                    SalesTax = table.Column<decimal>(nullable: false),
+                    Total = table.Column<decimal>(nullable: false),
+                    userEmail = table.Column<string>(nullable: true),
+                    DiscountCouponCode = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderID);
+                    table.ForeignKey(
+                        name: "FK_Orders_Discounts_DiscountCouponCode",
+                        column: x => x.DiscountCouponCode,
+                        principalTable: "Discounts",
+                        principalColumn: "CouponCode",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Orders_Users_userEmail",
+                        column: x => x.userEmail,
+                        principalTable: "Users",
+                        principalColumn: "Email",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    ReviewID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Rating = table.Column<decimal>(nullable: false),
+                    Approved = table.Column<bool>(nullable: false),
+                    ReviewDescription = table.Column<string>(nullable: true),
+                    AuthorEmail = table.Column<string>(nullable: true),
+                    ApproverEmail = table.Column<string>(nullable: true),
+                    BookID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.ReviewID);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Users_ApproverEmail",
+                        column: x => x.ApproverEmail,
+                        principalTable: "Users",
+                        principalColumn: "Email",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Users_AuthorEmail",
+                        column: x => x.AuthorEmail,
+                        principalTable: "Users",
+                        principalColumn: "Email",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Reviews_Books_BookID",
+                        column: x => x.BookID,
+                        principalTable: "Books",
+                        principalColumn: "BookID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderDetails",
+                columns: table => new
+                {
+                    OrderDetailID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    OrderQuantity = table.Column<int>(nullable: false),
+                    OrderDetailPrice = table.Column<int>(nullable: false),
+                    OrderID = table.Column<int>(nullable: true),
+                    BookID = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderDetails", x => x.OrderDetailID);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Books_BookID",
+                        column: x => x.BookID,
+                        principalTable: "Books",
+                        principalColumn: "BookID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_OrderDetails_Orders_OrderID",
+                        column: x => x.OrderID,
+                        principalTable: "Orders",
+                        principalColumn: "OrderID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_InventoryID",
+                table: "Books",
+                column: "InventoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_BookID",
+                table: "OrderDetails",
+                column: "BookID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderDetails_OrderID",
+                table: "OrderDetails",
+                column: "OrderID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_DiscountCouponCode",
+                table: "Orders",
+                column: "DiscountCouponCode");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_userEmail",
+                table: "Orders",
+                column: "userEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_ApproverEmail",
+                table: "Reviews",
+                column: "ApproverEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_AuthorEmail",
+                table: "Reviews",
+                column: "AuthorEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_BookID",
+                table: "Reviews",
+                column: "BookID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_InventoryID",
+                table: "Users",
+                column: "InventoryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderDetails");
+
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Discounts");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Inventories");
+        }
+    }
+}
