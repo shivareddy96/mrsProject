c51c4ca33b3622b834262400d1c4648372e778a1
matthewmartinez24 <43081613+matthewmartinez24@users.noreply.github.com>
2018-12-04 22:17:42 -0600

    Create AppDbContext.cs

---
+++ mrsProject/DAL/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using mrsProject.Models;
+
+namespace mrsProject.DAL
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+      
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Discount> Discounts { get; set; }
+
+        public DbSet<Inventory> Inventories { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+
+        public DbSet<Genre> Genres {get; set;}
+    }
+}
