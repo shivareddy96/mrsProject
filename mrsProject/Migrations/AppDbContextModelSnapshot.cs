b38fc030bd4701248162c0b5c528123232a3c3af
shivareddy96 <42914152+shivareddy96@users.noreply.github.com>
2018-12-05 17:07:10 -0600

    Update AppDbContextModelSnapshot.cs

--- mrsProject/Migrations/AppDbContextModelSnapshot.cs
+++ mrsProject/Migrations/AppDbContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace mrsProject.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.1.4-rtm-31024")
+                .HasAnnotation("ProductVersion", "2.1.1-rtm-30846")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
@@ -316,13 +316,11 @@ namespace mrsProject.Migrations
 
                     b.Property<int>("OrderNumber");
 
+                    b.Property<int>("PaymentMethod");
+
                     b.Property<bool>("PendingOrder");
 
                     b.Property<decimal>("ShippingCost");
-
-                    b.Property<decimal>("ShippingCostFirstBook");
-
-                    b.Property<decimal>("ShippingCostNextBook");
 
                     b.Property<string>("userId");
 
