65abcbedadb5a935b51df60e2d594a05d4d0a645
matthewmartinez24 <43081613+matthewmartinez24@users.noreply.github.com>
2018-12-06 15:03:41 -0600

    MigrationsFridayAt3

--- mrsProject/Migrations/20181206210215_UpdateSearch.Designer.cs
+++ mrsProject/Migrations/20181206210215_UpdateSearch.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using mrsProject.DAL;
 
 namespace mrsProject.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181206210215_UpdateSearch")]
+    partial class UpdateSearch
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ mrsProject/Migrations/20181206210215_UpdateSearch.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mrsProject.Migrations
+{
+    public partial class UpdateSearch : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "DiscountNum",
+                table: "Discounts",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumPurchased",
+                table: "Books",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DiscountNum",
+                table: "Discounts");
+
+            migrationBuilder.DropColumn(
+                name: "NumPurchased",
+                table: "Books");
+        }
+    }
+}
