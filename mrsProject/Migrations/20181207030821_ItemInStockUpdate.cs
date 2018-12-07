ba9b6e56dfc9ce4b618e01e71aa70af538eb2a4b
matthewmartinez24 <43081613+matthewmartinez24@users.noreply.github.com>
2018-12-06 21:11:53 -0600

    ItemInStockUpdate

---
+++ mrsProject/Migrations/20181207030821_ItemInStockUpdate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mrsProject.Migrations
+{
+    public partial class ItemInStockUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Pending",
+                table: "Reviews",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Pending",
+                table: "Reviews");
+        }
+    }
+}
