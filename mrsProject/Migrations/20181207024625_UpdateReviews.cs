9a5abc1de8ad986e4765ac038d4d3192545d3e44
matthewmartinez24 <43081613+matthewmartinez24@users.noreply.github.com>
2018-12-06 20:48:16 -0600

    MigrationsAt848

---
+++ mrsProject/Migrations/20181207024625_UpdateReviews.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace mrsProject.Migrations
+{
+    public partial class UpdateReviews : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReviewDescription",
+                table: "Reviews",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ReviewDescription",
+                table: "Reviews",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100,
+                oldNullable: true);
+        }
+    }
+}
